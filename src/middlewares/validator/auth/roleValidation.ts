a8349c5a25b17884d1b97d6cdcc5f021841ea528
Mochamad Nurul Huda <mochamadnurulhuda16@gmail.com>
2024-08-09 17:45:06 +0700

    fix: role validation

--- src/middlewares/validator/auth/roleValidation.ts
+++ src/middlewares/validator/auth/roleValidation.ts
@@ -7,5 +7,5 @@ export const validateRoles = [
     .isString()
     .withMessage("Name must be a string"),
   body("slug").optional().isString().withMessage("Slug must be a string"),
-  body("permissions").optional().isJSON().withMessage("Slug must be a JSON"),
+  body("permissions").optional().isArray().withMessage("Permission must be an Array"),
 ];
